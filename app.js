3905e7833b3d226a060836057fa393b3c0ce5da4
Fariz Ali <farizali23500@gmail.com>
2021-09-06 17:33:35 +0530

    third commit

--- app.js
+++ app.js
@@ -8,21 +8,48 @@ app.post("/",function(req,res){
     const symbol=req.body.symbol;
     const start=req.body.start;
     const end=req.body.end;
+    // const symbol="GOOG";
+    // const start="2021-01-12";
+    // const end="2021-02-01";
     const requiredData=[];
     // console.log(Number(start.substr(8,2)));
     data.forEach(d => {
-       if(Number(d.date.substr(5,2))>Number(end.substr(5,2))&&Number(d.date.substr(8,2))>Number(end.substr(8,2)))return;
-        if(d.symbol===symbol&&Number(d.date.substr(5,2))>=Number(start.substr(5,2))&&Number(d.date.substr(8,2))>=Number(start.substr(8,2)) && Number(d.date.substr(5,2))<=Number(end.substr(5,2))&&Number(d.date.substr(8,2))<=Number(end.substr(8,2)))
+    //    if(Number(d.date.substr(5,2))>Number(end.substr(5,2))&&Number(d.date.substr(8,2))>Number(end.substr(8,2)))return;
+        if(d.symbol===symbol&&Number(d.date.substr(5,2))>=Number(start.substr(5,2))&& Number(d.date.substr(5,2))<=Number(end.substr(5,2)))
        { 
-           
-        requiredData.push({"symbol":d.symbol,
-        "open": d.open,
-        "close":d.close,
-        "high":d.high,
-        "low":d.low,
-        "date":d.date
-
-    });}
+           if(Number(d.date.substr(8,2))>=Number(start.substr(8,2)) &&Number(d.date.substr(5,2))===Number(start.substr(5,2)))
+           {
+            requiredData.push({"symbol":d.symbol,
+            "open": d.open,
+            "close":d.close,
+            "high":d.high,
+            "low":d.low,
+            "date":d.date
+    
+        });
+           }
+           else if(Number(d.date.substr(8,2))<=Number(end.substr(8,2)) &&Number(d.date.substr(5,2))===Number(end.substr(5,2)))
+           {
+            requiredData.push({"symbol":d.symbol,
+            "open": d.open,
+            "close":d.close,
+            "high":d.high,
+            "low":d.low,
+            "date":d.date
+    
+        });
+           }
+           else if(Number(d.date.substr(5,2))!==Number(end.substr(5,2))&&Number(d.date.substr(5,2))!==Number(start.substr(5,2)) ){
+            requiredData.push({"symbol":d.symbol,
+            "open": d.open,
+            "close":d.close,
+            "high":d.high,
+            "low":d.low,
+            "date":d.date
+    
+        });
+           }
+      }
     });
    res.send(requiredData); 
 });
