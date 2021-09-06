721e080f193b399a56f3dbff1c360f8b9ef381c1
Fariz Ali <farizali23500@gmail.com>
2021-09-06 16:56:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const app=express();
+const data=require('./StockList.json');
+
+
+app.get("/",function(req,res){
+    const symbol="GOOG";
+    const start="2021-01-04";
+    const end="2021-01-11";
+    const requiredData=[];
+    // console.log(Number(start.substr(8,2)));
+    data.forEach(d => {
+       if(Number(d.date.substr(5,2))>Number(end.substr(5,2))&&Number(d.date.substr(8,2))>Number(end.substr(8,2)))return;
+        if(d.symbol===symbol&&Number(d.date.substr(5,2))>=Number(start.substr(5,2))&&Number(d.date.substr(8,2))>=Number(start.substr(8,2)) && Number(d.date.substr(5,2))<=Number(end.substr(5,2))&&Number(d.date.substr(8,2))<=Number(end.substr(8,2)))
+       { 
+           
+        requiredData.push({"symbol":d.symbol,
+        "open": d.open,
+        "close":d.close,
+        "high":d.high,
+        "low":d.low,
+        "date":d.date
+
+    });}
+    });
+   res.send(requiredData); 
+});
+
+
+
+
+
+
+
+
+
+app.listen(3000,function(){
+    console.log("Server started on port 3000");
+})
